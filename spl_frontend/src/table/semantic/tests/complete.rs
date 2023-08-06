335e957bcfd3bc0b63a1e25d183dbea93e481f0f
Alec Ghost <alec.ghost@protonmail.com>
2023-08-06 08:41:30 +0200

    fix: add test programs
    Put test programs into a file inside this repo
    and use this file for the tests.

--- spl_frontend/src/table/semantic/tests/complete.rs
+++ spl_frontend/src/table/semantic/tests/complete.rs
@@ -8,21 +8,28 @@ fn main() {
 
 #[test]
 fn test1() {
-    let test1 = std::fs::read_to_string("/Users/alex/dev/compiler/programs/test1.spl").unwrap();
-    let errors = test(&test1);
+    let file = std::fs::read_to_string("tests/programs/test1.spl").unwrap();
+    let errors = test(&file);
     eq!(errors, Vec::new());
 }
 
 #[test]
 fn test2() {
-    let test2 = std::fs::read_to_string("/Users/alex/dev/compiler/programs/test2.spl").unwrap();
-    let errors = test(&test2);
+    let file = std::fs::read_to_string("tests/programs/test2.spl").unwrap();
+    let errors = test(&file);
     eq!(errors, Vec::new());
 }
 
 #[test]
 fn acker() {
-    let acker = std::fs::read_to_string("/Users/alex/dev/compiler/programs/acker.spl").unwrap();
-    let errors = test(&acker);
+    let file = std::fs::read_to_string("tests/programs/acker.spl").unwrap();
+    let errors = test(&file);
+    eq!(errors, Vec::new());
+}
+
+#[test]
+fn bigtest() {
+    let file = std::fs::read_to_string("tests/programs/bigtest.spl").unwrap();
+    let errors = test(&file);
     eq!(errors, Vec::new());
 }
